ac7de11ee00370b55d905ad6f2ae2e77c1840874
Adam Matthiesen <amatthiesen@outlook.com>
2026-05-21 23:23:37 -0700

    Refactor build configuration to use plugin aliases for improved readability

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.gradleup.shadow") version "9.3.1" apply false
-    id("dev.architectury.loom") version("1.13-SNAPSHOT") apply false
-    id("architectury-plugin") version("3.4-SNAPSHOT") apply false
+    alias(libs.plugins.shadow) apply false
+    alias(libs.plugins.architectury.loom) apply false
+    alias(libs.plugins.architectury.plugin) apply false
 }
 
 
