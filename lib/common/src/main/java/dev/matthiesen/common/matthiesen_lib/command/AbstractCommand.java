33ed272b4c6ea2aabb330e008c1e1157edcc3e06
Adam Matthiesen <amatthiesen@outlook.com>
2026-05-31 19:14:22 -0700

    Removes previously deprecated functions and methods

--- lib/common/src/main/java/dev/matthiesen/common/matthiesen_lib/command/AbstractCommand.java
+++
@@ -1,8 +0,0 @@
-package dev.matthiesen.common.matthiesen_lib.command;
-
-/** @deprecated Use {@link dev.matthiesen.common.matthiesen_lib_api.command.AbstractCommand} instead. */
-@Deprecated(forRemoval = true)
-@SuppressWarnings("unused")
-public abstract class AbstractCommand extends dev.matthiesen.common.matthiesen_lib_api.command.AbstractCommand {
-    public AbstractCommand() {}
-}
--- lib/common/src/main/java/dev/matthiesen/common/matthiesen_lib/core/interfaces/MatthiesenLibTextParser.java
+++
@@ -1,9 +0,0 @@
-package dev.matthiesen.common.matthiesen_lib.core.interfaces;
-
-/**
- * @deprecated Use {@link MatthiesenLibExtendedTextParser} for Embers-aware parsing, or
- * {@link dev.matthiesen.common.matthiesen_lib_api.core.interfaces.MatthiesenLibTextParser} for server-safe parsing.
- */
-@Deprecated(forRemoval = true)
-public interface MatthiesenLibTextParser extends MatthiesenLibExtendedTextParser {
-}
